9a652f6481ea0d79be4a5d6a32e4223e6ba87e96
ndeu <gautamupen99@gmail.com>
2021-08-24 16:31:54 +0545

    Turtle In-Position

---
+++ ProjectTurtle.py
@@ -0,0 +1,55 @@
+import turtle as T
+import time as t
+import random as r
+
+COLOR = ["red","green","blue","orange","yellow","black","pink",'lime','olive','seagreen']
+
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input("Enter the number of racers in the game(2 - 10): ")
+        print("\n\n")
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print("Invalid input!!Please enter a character")
+            print("\n\n")
+            continue
+        
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print("Too many racers!! Enter something small")
+            print("\n\n")
+
+def Screen():
+
+    WIDTH = 500
+    HEIGHT = 500
+    T.Screen().setup(WIDTH,HEIGHT)
+    T.Screen().title("TurtleRace")
+
+def turtle_racers(no):
+    turtle_no = []
+    x = -230; y = -230
+
+    for a,b in enumerate(no):
+        racers = T.Turtle()
+        racers.color(b)
+        racers.shape("turtle")
+        racers.left(90)
+        racers.penup()
+        racers.setpos(x , y)
+        x += (480 // len(colors))
+        racers.pendown()
+        turtle_no.append(racers)
+
+
+
+
+Screen()
+racers = get_number_of_racers()
+
+r.shuffle(COLOR)
+colors = COLOR[:racers]
+turtle_racers(colors)
